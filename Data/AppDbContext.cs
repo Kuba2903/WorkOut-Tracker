4ed7b46051a1e01101b86b504ecdf0e93b7ca3eb
Kuba2903 <jakub.adamczyk@microsoft.wsei.edu.pl>
2024-08-20 20:53:20 -0500

    create exercise entity

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Exercise> Exercises { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseSqlServer("Data Source=HP;Initial Catalog=ExerciseDb;Integrated Security=True;Trust Server Certificate=True");
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}

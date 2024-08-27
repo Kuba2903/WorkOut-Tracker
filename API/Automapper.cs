642405a4b1f4cb803bd92fa52df26cd4826ba843
Kuba2903 <jakub.adamczyk@microsoft.wsei.edu.pl>
2024-08-26 19:07:51 -0500

    create exercise endpoint

--- API/Automapper.cs
+++ API/Automapper.cs
@@ -1,13 +1,20 @@
 ﻿using AutoMapper;
+using Data;
 using Data.DTO_s;
 using Data.Entities;
+using Microsoft.EntityFrameworkCore;
 namespace API
 {
     public class Automapper : Profile 
     {
+
         public Automapper()
         {
-            CreateMap<ExerciseDTO, Exercise>();
+            CreateMap<ExerciseDTO, Exercise>()
+            .ForMember(dest => dest.Category, opt => opt.Ignore())
+            .ForMember(dest => dest.CategoryId, opt => opt.Ignore())
+            .ForMember(dest => dest.WorkoutExercises, opt => opt.Ignore());
         }
+
     }
 }
--- API/Controllers/ExerciseController.cs
+++ API/Controllers/ExerciseController.cs
@@ -1,7 +1,11 @@
-﻿using Data;
+﻿using AutoMapper;
+using Data;
 using Data.DTO_s;
+using Data.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 namespace API.Controllers
 {
@@ -10,10 +14,11 @@ namespace API.Controllers
     public class ExerciseController : ControllerBase
     {
         private readonly AppDbContext dbContext;
-        
-        public ExerciseController(AppDbContext dbContext)
+        private readonly IMapper mapper;
+        public ExerciseController(AppDbContext dbContext, IMapper mapper)
         {
             this.dbContext = dbContext;
+            this.mapper = mapper;
         }
 
         [HttpPost]
@@ -21,9 +26,50 @@ namespace API.Controllers
 
         public async Task<IActionResult> Create(ExerciseDTO dto)
         {
-            
+            var entity = mapper.Map<Exercise>(dto);
+
+            entity.Weight = CheckNulls(dto.Weight);
+            entity.Sets = CheckNulls(dto.Sets);
+            entity.Repetition = CheckNulls(dto.Repetition);
+
+            if (dto.Description.IsNullOrEmpty())
+                entity.Description = null;
+
+
+            var category = await dbContext.Categories.FirstOrDefaultAsync(x => x.Name == dto.Category);
+
+            if (category == null)
+            {
+                var newCategory = new Category()
+                {
+                    Name = dto.Category
+                };
+                await dbContext.Categories.AddAsync(newCategory);
+                await dbContext.SaveChangesAsync();
+                entity.CategoryId = newCategory.Id;
+            }
+            else
+            {
+                entity.CategoryId = category.Id;
+            }
+
+            await dbContext.Exercises.AddAsync(entity);
+            await dbContext.SaveChangesAsync();
 
             return Ok();
+        }
+
+
+        private static int? CheckNulls(int? x)
+        {
+            if (x == 0)
+            {
+                return null;
+            }
+            else
+            {
+                return x;
+            }
         }
     }
 }
